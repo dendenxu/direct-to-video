027e9fb6ee9126bde09dfee0d759b7d6cf26ebb4
xuzhen <zhenx@zju.edu.cn>
2025-12-22 17:00:34 +0800

    switch to using default gop size

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -160,14 +160,14 @@ atg_dtv::Encoder::Error addStream(atg_dtv::OutputStream *ost,
             ost->av_stream->time_base = AVRational{1, settings.frameRate};
             codecContext->time_base = ost->av_stream->time_base;
 
-            codecContext->gop_size = 12;
             codecContext->pix_fmt = AV_PIX_FMT_YUV420P;
 
-            if (codecContext->codec_id == AV_CODEC_ID_MPEG2VIDEO) {
-                codecContext->max_b_frames = 2;
-            } else if (codecContext->codec_id == AV_CODEC_ID_MPEG1VIDEO) {
-                codecContext->mb_decision = 2;
-            }
+            // codecContext->gop_size = 12;
+            // if (codecContext->codec_id == AV_CODEC_ID_MPEG2VIDEO) {
+            //     codecContext->max_b_frames = 2;
+            // } else if (codecContext->codec_id == AV_CODEC_ID_MPEG1VIDEO) {
+            //     codecContext->mb_decision = 2;
+            // }
             break;
         default:
             return Error::UnsupportedMediaType;
